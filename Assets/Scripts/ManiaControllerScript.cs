d4ae6b40d536d71f77778680f583f30523cd446c
GMartin55555 <161363076+GMartin55555@users.noreply.github.com>
2025-02-11 15:35:43 +0000

    added manina and manina screen lean

--- Assets/Scripts/ManiaControllerScript.cs
+++ Assets/Scripts/ManiaControllerScript.cs
@@ -1,18 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class ManiaControllerScript : MonoBehaviour
 {
+    public float maniaScore;
+    private float maniaMultiplier;
+    private float maniaSoftCap = 100f;
+    [SerializeField] private bool allowManiaPassive;
+
+    [SerializeField] private TMP_Text maniaScoreText;
+    [SerializeField] private Image maniaBar;
+    [SerializeField] private Slider maniaSlider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        maniaScore = 0;
+        maniaMultiplier = 1;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            SetManiaScore(0);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SetManiaScore(100);
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            if (allowManiaPassive)
+            {
+                allowManiaPassive = false;
+            }
+            else
+            {
+                allowManiaPassive = true;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (maniaScore <= maniaSoftCap)
+        {
+            ManiaGrowth(0.1f);
+        }
+
+        maniaScoreText.text = maniaScore.ToString();
+        ManiaDisplay();
+    }
+
+    private void ManiaGrowth(float growth)
+    {
+        if(allowManiaPassive)
+        {
+            maniaScore += growth * maniaMultiplier;
+        }
+    }
+
+    private void ManiaDecay(float decay)
+    {
+        maniaScore -= decay;
+    }
+
+    private void SetManiaScore(float value)
+    {
+        maniaScore = value;
+    }
+
+    public void ManiaDisplay()
+    {
+        maniaSlider.value = maniaScore;
     }
 }
--- Assets/Scripts/PlayerScripts/PlayerCameraController.cs
+++ Assets/Scripts/PlayerScripts/PlayerCameraController.cs
@@ -7,14 +7,19 @@ public class PlayerCameraController : MonoBehaviour
 {
     [SerializeField] private float mouseSensitivityX = 100f;
     [SerializeField] private float mouseSensitivityY = 100f;
+    [SerializeField] private ManiaControllerScript mania;
+    [SerializeField] private float leanSpeed = 5f;
 
+    [SerializeField] private new Camera camera;
     public Transform orientation;
 
     private float xRoation;
     private float yRoation;
+    private float leanValue;
 
-    [Header ("InputActions")]
+    [Header("InputActions")]
     [SerializeField] private InputActionReference lookAction;
+    [SerializeField] private InputActionReference moveAction;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +29,7 @@ public class PlayerCameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float mouseX = lookAction.action.ReadValue<Vector2>().x * Time.deltaTime * mouseSensitivityX;
         float mouseY = lookAction.action.ReadValue<Vector2>().y * Time.deltaTime * mouseSensitivityY;
@@ -33,8 +38,28 @@ public class PlayerCameraController : MonoBehaviour
         xRoation -= mouseY;
 
         xRoation = Mathf.Clamp(xRoation, -90f, 90f);
+        ManiaLean();
+        camera.transform.rotation = Quaternion.Euler(xRoation, yRoation, leanValue);
+        transform.rotation = Quaternion.Euler(0f, yRoation, 0f);
+    }
 
-        transform.rotation = Quaternion.Euler(xRoation, yRoation, 0f);
-        orientation.rotation = Quaternion.Euler(0f, yRoation, 0f);
+    private void ManiaLean()
+    {
+        float desiredLeanValue = 0;
+        if (moveAction.action.ReadValue<Vector2>().x > 0)
+        {
+            desiredLeanValue = -mania.maniaScore / 5;
+        }
+        else if (moveAction.action.ReadValue<Vector2>().x < 0)
+        {
+            desiredLeanValue = mania.maniaScore / 5;
+        }
+        else
+        {
+            desiredLeanValue = 0;
+        }
+
+        leanValue = Mathf.Lerp(leanValue, desiredLeanValue, Time.deltaTime * leanSpeed);
+
     }
 }
